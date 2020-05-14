5825a8a33df54f4a6f885e482bc3e5a082cb88c5
Andy Kogut <andriik@wix.com>
2020-05-14 19:27:19 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ const UndefSymbol = Symbol();
 type Undef = typeof UndefSymbol;
 type NonUndef<T> = T extends Undef ? never : T;
 
-/* tslint:disable prettier */
 type Converted<T> = {
   [P in keyof T]:
     Undef extends T[P] ? NonUndef<T[P]> | undefined :
@@ -21,7 +20,6 @@ type DefaultsDeep<T, U extends Partial<T>> = {
     undefined extends T[P] ? T[P] | Undef :
     T[P]
 };
-/* tslint:enable prettier */
 
 export default <T, U>(object: T, defaults: U): Converted<DefaultsDeep<T, U>> =>
   lodashDefaultsDeep(object, defaults);
